07cb65af41024845481f52a2ac7a1455f3bbf63e
liche <lichee55@gmail.com>
2021-06-14 21:46:35 +0900

    [Update] board controller update

--- Selfcare/src/controller/BoardController.java
+++ Selfcare/src/controller/BoardController.java
@@ -13,25 +13,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import domain.Board;
+import service.BoardService;
 
 @WebServlet(name = "boardController", urlPatterns = "/board/*")
 public class BoardController extends HttpServlet {
-	
+
 	private final BoardService boardService = new BoardService();
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String uri = request.getRequestURI();
 		String conPath = request.getContextPath();
-		conPath+="/board";
+		conPath += "/board";
 		String com = uri.substring(conPath.length());
 		System.out.println("============BoardController=============");
-		System.out.println("uri "+ uri);
-		System.out.println("conPath "+ conPath);
-		System.out.println("com "+ com);
-		
-		if(com.equals("/")) {
-			ArrayList<Board> boards=boardService
+		System.out.println("uri " + uri);
+		System.out.println("conPath " + conPath);
+		System.out.println("com " + com);
+
+		if (com.equals("/list")) {
+			String boardPage = request.getParameter("page");
+			if (boardPage == null) {
+				System.out.println("no page param");
+				boardPage = "1";
+			}
+			System.out.println(boardPage);
+			ArrayList<Board> boards = boardService.findBoards(Integer.parseInt(boardPage));
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("/board/list");
+			mv.getModel().put("boards", boards);
+			String viewPath = viewResolver(mv.getViewName());
+			View view = new View(viewPath);
+			view.render(mv.getModel(), request, response);
 		}
+	}
+
+	private String viewResolver(String viewName) {
+		return "/WEB-INF/view/" + viewName + ".jsp";
 	}
 }
