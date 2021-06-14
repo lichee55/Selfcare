78aa31a77f77f48592028d996961e5bd0037a241
wlstjd <xkakrlfh@naver.com>
2021-06-14 17:49:12 +0900

    zz

--- Selfcare/src/service/MemberService.java
+++ Selfcare/src/service/MemberService.java
@@ -5,5 +5,5 @@ import persistence.MemberRepository;
 public class MemberService {
 	private final MemberRepository memberRepository = MemberRepository.getInstance();
 	public MemberService() {}
-	
+	int zz=0;
 }
